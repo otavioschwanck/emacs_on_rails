362b6aa2e8bfb811e89a52d108a9d00f38a31247
Otávio Schwanck <otavioschwanck@gmail.com>
2020-02-10 22:39:13 +0000

    Javascript tern

--- lisp/javascript.el
+++ lisp/javascript.el
@@ -3,3 +3,24 @@
   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
   (add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))
   )
+
+(use-package js2-refactor
+  :config
+  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  (js2r-add-keybindings-with-prefix "C-c C-r")
+  (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+  )
+
+(use-package xref-js2
+  :config
+  (add-hook 'js2-mode-hook (lambda ()
+                           (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+  )
+
+(use-package company-tern
+  :config
+  (add-to-list 'company-backends 'company-tern)
+  (add-hook 'js2-mode-hook (lambda ()
+                           (tern-mode)
+                           (company-mode)))
+  )
